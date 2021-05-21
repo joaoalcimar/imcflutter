a965dc37a800859a2824056de0ff9438c5d5940b
Joao Alcimar <joao.alcimar.junior@gmail.com>
2021-05-21 14:58:37 -0300

    Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home()
-  ) );
+  runApp(MaterialApp(home: Home()));
 }
 
 class Home extends StatefulWidget {
@@ -19,8 +18,44 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text("IMC Calculator"),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(onPressed: () {}, icon: Icon(Icons.refresh))
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_outline, size: 120, color: Colors.green),
+            TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Weight(Kg)",
+                    labelStyle: TextStyle(color: Colors.green)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0)),
+            TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Height(cm)",
+                    labelStyle: TextStyle(color: Colors.green)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0)),
+            Container(
+                height: 50.0,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Calculate",
+                        style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.green))))
+          ],
+        ),
       ),
     );
   }
 }
-
